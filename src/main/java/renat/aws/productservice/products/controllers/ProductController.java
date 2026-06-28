aeb2479d52641528d303a3231760c01c96e95759
Renat Sim <renat.tat@mail.ru>
2026-06-28 14:40:22 +0300

    Creation first stub controller ProductController

---
+++ src/main/java/renat/aws/productservice/products/controllers/ProductController.java
@@ -0,0 +1,21 @@
+package renat.aws.productservice.products.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private static final Logger LOG = LogManager.getLogger(ProductController.class);
+
+    @GetMapping
+    public String getAllProducts(){
+        LOG.info("Get all products");
+        return "All products";
+    }
+
+}
